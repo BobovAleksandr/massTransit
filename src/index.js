b0549a66880f8329bb7f5e5141634f64027fb962
Aleksandr Bobov <bobov.aleksandr.fr@yandex.ru>
2024-10-14 22:51:56 +0500

    Начинаем JS с нуля

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import './styles/index.css'
 import readXlsxFile from 'read-excel-file'
 import { shopList } from './shops.js'
 
-const inputAddFile = document.querySelector('.input-file-add')
+const inputLoadFile = document.querySelector('.input-file-add')
 const shopListRowTemplate = document.querySelector('#shopListRowTemplate').content
 const tableShops = document.querySelector('.table-shop-list')
-const optionShopRecieverTemplate = document.querySelector('#shopRecieverOptionTemplate').content
 
 const inputItemId = document.querySelector('.input-item-id')
+const optionShopRecieverTemplate = document.querySelector('#shopRecieverOptionTemplate').content
 const selectShopReciever = document.querySelector('.select-shop-reciever')
 
 const formAuto = document.forms['form-auto']
@@ -20,162 +20,51 @@ const checkboxDontMakeEmpty = formAuto.elements['checkbox-item-dont-to-make-empt
 const shippingListRowTemplate = document.querySelector('#resultRowTemplate').content
 const tableResult = document.querySelector('.table-result')
 
+const recievers = [
+  'Пермь ТК Чкаловский ТП',
+  'Пермь Космонавтов ТП',
+  'Пермь ТЦ Лайнер ТП',
+  'Пермь Склад',
+  'Екатеринбург Склад',
+]
+
 // Заполняет магазины количеством товара из таблицы
 function fillShopsWithValues(data) {
-  const currentShopsArray = data.filter(shop => shop[0] !== '#ERROR_undefined' && shop[1] !== '#ERROR_undefined' && shop[2] !== '#ERROR_undefined')
-  shopList.forEach(shopObject => {
-    const currentShop = currentShopsArray.find(shop => shop[1] == shopObject.name)
-    if (currentShop) {
-      shopObject.itemValue = currentShop[2]
-    }
-  })
-}
-
-// Заполняем магазин количеством новых товаров (количество > 1 или Технопоинт или Склад)
-function fillShopsWithSpecificItems(shop) {
-  if (shop.type === 'Склад') {
-    shop.notForEmptyValue = shop.itemValue - 1
-  } else {
-    shop.notForEmptyValue = Math.floor(shop.itemValue / 2)
-  }
-  if (shop.type === 'Технопоинт' || shop.type === 'Склад') {
-    shop.newItemValue = shop.itemValue
-  } else if (shop.itemValue > 1) {
-    shop.newItemValue = shop.itemValue - 1
-  }
-}
-
-function fillShopsWithRerenderFunction() {
   shopList.forEach(shop => {
-    shop.renderResultTable = renderNewResultRow
+    shop.fillTotalValue(data)
+    shop.fillItemSpecificValues
   })
 }
-fillShopsWithRerenderFunction()
-
-// Заполняет элемент выбора магазина-получателя опциями
-function fillShopRecieverSelect() {
+  
+// Рендерит таблицу со списком магазинов (остаток > 0)
+function renderShopTable() {
   shopList.forEach(shop => {
-    if ('createShopRecieverOption' in shop) {
-      selectShopReciever.append(shop.createShopRecieverOption(optionShopRecieverTemplate))
+    if (shop.itemTotalValue > 0) {
+      tableShops.append(
+        shop.createShopListRowElement(shopListRowTemplate)
+      )
     }
   })
 }
-fillShopRecieverSelect()
-
-function filterShipping() {
-  // Исключаем магазины без товара
-  let shippingList = shopList.filter(shop => shop.itemValue > 0)
-  // resetCurrentShipValue()
-  shippingList.forEach(shop => shop.fillToShipValue(shop.itemValue))
-  // Исключаем филиал получатель
-  if (selectShopReciever.value) {
-    const currentReciever = shopList.find(shop => shop.name === selectShopReciever.value)
-    shippingList = shippingList.filter(shop => shop.name !== currentReciever.name)
-    shippingList.forEach(shop => shop.fillToShipValue(shop.itemValue))
-  }
-  // Исключаем магазины с витринными товарами
-  if (checkboxItemNew.checked) {
-    shippingList = shippingList.filter(shop => shop.newItemValue > 0)
-    shippingList.forEach(shop => shop.fillToShipValue(shop.newItemValue))
-  }
-  // Исключаем склады
-  if (checkboxNotWarehouse.checked) {
-    shippingList = shippingList.filter(shop => shop.type !== 'Склад')
-  }
-  if (checkboxDontMakeEmpty.checked) {
-    shippingList = shippingList.filter(shop => shop.notForEmptyValue > 0)
-    shippingList.forEach(shop => shop.fillToShipValue(shop.notForEmptyValue))
-  }
-  console.log(shippingList)
-  return shippingList
-}
-
-function disableButton(inputIsEmpty) {
-  if (inputIsEmpty) {
-    formAutoSubmitButton.disabled = true
-    formAutoSubmitButton.classList.add('disabled')
-  } else {
-    formAutoSubmitButton.disabled = false
-    formAutoSubmitButton.classList.remove('disabled')
-  }
-}
-
-function uncheckShipOption(evt) {
-  if (evt.target.checked) {
-    evt.target === checkboxItemNew ? checkboxDontMakeEmpty.checked = false : checkboxItemNew.checked = false
-  }
-}
-
-function clearResultTable() {
-  const currentresultTable = [...tableResult.querySelectorAll('.table-row')]
-  currentresultTable.forEach(element => element.remove())
-}
-
-function resetCurrentShipValue() {
-  shopList.forEach(shop => shop.currentShipValue = 0)
-}
-
-function renderNewResultRow() {
-  clearResultTable()
-  filterShipping().forEach(shop => {
-    if (shop.currentShipValue > 0) {
-      const reciever = shopList.find(shop => shop.isCurrentReciever === true) ? shopList.find(shop => shop.isCurrentReciever === true) : ""
-      tableResult.append(shop.createShippingListRowElement(shippingListRowTemplate, reciever.id, inputItemId.value))
-    }
+  
+// Рендерит опции выбора филиала-получателя
+function renderRecieveroptions(recievers, template) {
+  const filteredShoplist = shopList.filter(shop => recievers.includes(shop.name))
+  filteredShoplist.forEach(shop => {
+    selectShopReciever.append(
+      shop.createShopRecieverOption(template)
+    )
   })
 }
 
-selectShopReciever.addEventListener('change', evt => {
-  shopList.forEach(shop => shop.makeThisShopReciever(false))
-  const newShopReciever = shopList.find(shop => shop.name === evt.target.value)
-  const recieverElement = [...tableShops.querySelectorAll('.table-data-shop-name')].find(element => element.textContent === newShopReciever.name)
-  recieverElement.closest('.table-row').querySelector('.input-shipping-value').value = 0
-  newShopReciever.makeThisShopReciever(true)
-  filterShipping()
-  renderNewResultRow()
-})
-
-inputAddFile.addEventListener('change', () => {
-  readXlsxFile(inputAddFile.files[0])
+// Обработка excel-файла и зпуск функций после неё  
+inputLoadFile.addEventListener('change', () => {
+  readXlsxFile(inputLoadFile.files[0])
     .then(excelData => {
+      excelData.filter(shop => shop[0] !== '#ERROR_undefined' && shop[1] !== '#ERROR_undefined' && shop[2] !== '#ERROR_undefined')
       fillShopsWithValues(excelData)
-      shopList.forEach(shop => {
-        fillShopsWithSpecificItems(shop)
-        if (shop.itemValue > 0) {
-          tableShops.append(shop.createShopListRowElement(shopListRowTemplate))
-        }
-      })
+      renderShopTable()
+      renderRecieveroptions(recievers, optionShopRecieverTemplate)
     })
 })
-
-formAuto.addEventListener('submit', evt => {
-  evt.preventDefault()
-  clearResultTable()
-  resetCurrentShipValue()
-  let currentShippingValueTotal = 0
-  const currentShippingList = filterShipping()
-  const reciever = shopList.find(shop => shop.isCurrentReciever === true) ? shopList.find(shop => shop.isCurrentReciever === true) : ""
-  for (let i = 0; i < +inputItemValue.value; i++) {
-    // КОСЯК
-    const currentNotFullShop = currentShippingList.find(shop => shop.currentShipValue < shop.toShipValue)
-    currentNotFullShop.currentShipValue++
-    currentShippingValueTotal++
-    if (currentNotFullShop.currentShipValue === currentNotFullShop.toShipValue || currentShippingValueTotal === +inputItemValue.value) {
-      tableResult.append(currentNotFullShop.createShippingListRowElement(shippingListRowTemplate, reciever.id, inputItemId.value))
-    }
-  }
-})
-
-
-inputItemId.addEventListener('change', renderNewResultRow)
-
-inputItemValue.addEventListener('input', evt => {
-  let isValueEmpty
-  isValueEmpty = evt.target.value ? false : true
-  disableButton(isValueEmpty)
-})
-
-checkboxItemNew.addEventListener('change', uncheckShipOption)
-checkboxDontMakeEmpty.addEventListener('change', uncheckShipOption)
-
 
